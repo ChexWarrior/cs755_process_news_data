ac0719acccf4b3efd4cad69e5ff0fae58c63792d
Andrew Lehman <aplehm@gmail.com>
2019-11-16 12:49:49 -0500

    Return read_csv straight away

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
 def get_csv_data(file_path):
-    frame = pd.read_csv(file_path, usecols=['title', 'publication'])
-
-    return frame
+    return pd.read_csv(file_path, usecols=['title', 'publication'])
 
 new_sources = set()
 data_paths = [
     './articles1.csv',
-    './articles2csv',
+    './articles2.csv',
     './articles3.csv',
 ]
 
 # CSV Data as data frame
-data = get_csv_data('./articles1.csv')
+for path in data_paths:
+    data = get_csv_data(path)
 
-for row in data.iterrows():
-    # print(row[1]['publication'])
-    new_sources.add(row[1]['publication'])
+    for row in data.iterrows():
+        new_sources.add(row[1]['publication'])
 
 for publication in new_sources:
-    print(publication)
+        print(publication)
 
